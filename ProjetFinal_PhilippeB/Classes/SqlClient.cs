967ec2003455a5afa914674ce754b142a50860ba
Philippebertrand1 <2066487@cegepoutaouais.qc.ca>
2022-12-11 18:02:37 -0500

    Ajout d'une classe ado.Net pour le mode déconnecté

---
+++ ProjetFinal_PhilippeB/Classes/SqlClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace ProjetFinal_PhilippeB
+{
+    internal class SqlClient
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataAdapter adapter;
+        private string connectionString;
+        private DataSet dsBibliotheque;
+        private DataTable dtClient;
+
+        public SqlConnection Connection { get => connection; set => connection = value; }
+        public SqlCommand Command { get => command; set => command = value; }
+        public SqlDataAdapter Adapter { get => adapter; set => adapter = value; }
+        public string ConnectionString { get => connectionString; }
+        public DataSet DsBibliotheque { get => dsBibliotheque; set => dsBibliotheque = value; }
+        public DataTable DtClient { get => dtClient; set => dtClient = value; }
+        
+
+        public SqlClient()
+        {
+            connectionString = "Data Source=C-I5CGJJSJR5LS7;Initial Catalog=Bibliotheque;User ID=sa;Password=sql";
+            adapter = new SqlDataAdapter();
+            command = new SqlCommand();
+            connection = new SqlConnection(connectionString);
+            dsBibliotheque = new DataSet();
+            dtClient = new DataTable();
+        }
+    }
+}
--- ProjetFinal_PhilippeB/Classes/StaticListes.cs
+++ ProjetFinal_PhilippeB/Classes/StaticListes.cs
@@ -43,7 +43,7 @@ namespace ProjetFinal_PhilippeB
             lsLivresEmpruntes = new List<Livre> ();
         }
 
-
+       
     }
 
 
